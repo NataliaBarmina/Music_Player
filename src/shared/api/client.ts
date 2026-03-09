6aef302b19cb4b0f3e84ec63ca8c6b14d9a485af
NataliaBarmina <natbar73@mail.ru>
2026-03-09 17:17:55 +0300

    Fix schema import path

--- src/shared/api/client.ts
+++ src/shared/api/client.ts
@@ -1,5 +1,5 @@
 import createClient from 'openapi-fetch';
-import type { paths } from './api/schema';
+import type { paths } from './schema';
 
 export const client = createClient<paths>({
   baseUrl: 'https://musicfun.it-incubator.app/api/1.0/',
