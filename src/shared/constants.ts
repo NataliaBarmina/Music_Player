03e2f300b8279c5a38817cd99c99f8c7836ff635
NataliaBarmina <natbar73@mail.ru>
2026-03-10 15:17:37 +0300

    change api-key

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL = 'https://musicfun.it-incubator.app/api/1.0/playlists/tracks';
 export const API_HEADERS = {
-  'api-key': '2d979fd4-573b-4bca-aee7-e3e6cca27eee',
+  'api-key': '54e7a4af-fabe-4b92-831c-4d3b597611bd',
 };
