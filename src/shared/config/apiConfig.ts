8235a162e95fcd3539e65ab565d3693be9520156
NataliaBarmina <natbar73@mail.ru>
2026-06-09 15:18:00 +0300

    check API key in production build

--- src/shared/config/apiConfig.ts
+++ src/shared/config/apiConfig.ts
@@ -2,3 +2,7 @@ export const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const BASE_URL = import.meta.env.VITE_BASE_URL;
 export const CALLBACK_URL = import.meta.env.VITE_CALLBACK_URL;
+
+console.log('API key exists:', Boolean(API_KEY));
+console.log('API key start:', API_KEY?.slice(0, 4));
+console.log('API key end:', API_KEY?.slice(-4));
